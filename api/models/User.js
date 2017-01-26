29b9b8d3482e2cc3992b6be8d5931b8b0bce3f05
vnahim <v.nahim@gmail.com>
2017-01-26 13:35:16 +0100

    New update

--- api/models/User.js
+++ api/models/User.js
@@ -8,6 +8,28 @@
 var bcrypt = require('bcrypt');
 
 module.exports = {
+    index: function (req,res){
+
+        res.writeHead(200, {'content-type': 'text/html'});
+        res.end(
+            '<form action="http://localhost:1337/file/upload" enctype="multipart/form-data" method="post">'+
+            '<input type="text" name="title"><br>'+
+            '<input type="file" name="avatar" multiple="multiple"><br>'+
+            '<input type="submit" value="Upload">'+
+            '</form>'
+        )
+    },
+    upload: function  (req, res) {
+        req.file('avatar').upload({
+            dirname: './assets/images'
+        },function (err, uploadedFiles) {
+            if (err) return res.negotiate(err);
+
+            return res.json({
+                message: uploadedFiles.length + ' file(s) uploaded successfully!'
+            });
+        });
+    },
   attributes: {
     email: {
       type: 'email',
@@ -16,9 +38,26 @@ module.exports = {
     },
     password: {
       type: 'string',
-      minLength: 6,
       required: true
     },
+      codepostal: {
+          type: 'string',
+          required: true
+      },
+      adresse: {
+          type: 'string',
+          required: true
+      },
+      ville: {
+          type: 'string',
+          required: true
+      },
+      avatar: {
+          type: 'file',
+          required: true
+      },
+
+
     toJSON: function() {
       var obj = this.toObject();
       delete obj.password;
